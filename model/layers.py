[fix] bug fix and loss computation

model/layers.py
@@ -1,31 +1,61 @@
+import abc
 import tensorflow as tf
 import tensorflow.keras as K
 import tensorflow.keras.layers as L
 import numpy as np
 
+class CouplingBase(L.Layer, metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def inverse(self, inputs):
+        pass
 
-class AdditiveCoupling(L.Layer):
-    def __init__(self, m):
+    @property
+    @abc.abstractmethod
+    def log_det_jacobian(self):
+        pass
+
+
+class AdditiveCoupling(CouplingBase):
+    def __init__(self, parity, m):
         super(AdditiveCoupling, self).__init__()
         self.m = m
+        self.parity = parity
 
     def build(self, input_shape):
         assert input_shape[-1] % 2 == 0
-        self.split_size = input_shape[-1] // 2
+        self.data_dim = input_shape[-1]
+
+    @property
+    def log_det_jacobian(self):
+        return 0.
+
+    def _concat_alt(self, t1, t2):
+        # concat t1 and t2 in alternate fashion
+        # e.g., [t1_1, t2_1, t1_2, ...]
+        t1_ex = tf.expand_dims(t1, axis=2)
+        t2_ex = tf.expand_dims(t2, axis=2)
+        t = tf.concat([t1_ex, t2_ex], axis=2)
+        return tf.reshape(t, [-1, self.data_dim])
 
     def call(self, inputs):
-        x1, x2 = tf.split(inputs, [self.split_size, self.split_size], axis=-1)
+        x1, x2 = inputs[:, self.parity::2], inputs[:, 1 - self.parity::2]
         y1 = x1
         y2 = x2 + self.m(x1)
-        return tf.concat([y1, y2], axis=-1)
+        if self.parity == 0:
+            return self._concat_alt(y1, y2)
+        else:
+            return self._concat_alt(y2, y1)
     
     def inverse(self, inputs):
-        y1, y2 = tf.split(inputs, [self.split_size, self.split_size], axis=-1)
+        y1, y2 = inputs[:, self.parity::2], inputs[:, 1 - self.parity::2]
         x1 = y1
         x2 = y2 - self.m(y1)
-        return tf.concat([x1, x2], axis=-1)
+        if self.parity == 0:
+            return self._concat_alt(x1, x2)
+        else:
+            return self._concat_alt(x2, x1)
 
-class AffineCoupling(L.Layer):
+class AffineCoupling(CouplingBase):
     def __init__(self, parity, nn_s, nn_t):
         super(AffineCoupling, self).__init__()
         assert parity == 0 or parity == 1
@@ -33,6 +63,11 @@ class AffineCoupling(L.Layer):
         # nn for scale and translation
         self.nn_s = nn_s
         self.nn_t = nn_t
+        self._log_det_jacobian = 0.
+
+    @property
+    def log_det_jacobian(self):
+        return self._log_det_jacobian
 
     def build(self, input_shape):
         # checker board masks
@@ -51,11 +86,7 @@ class AffineCoupling(L.Layer):
         y = x_masked + (1. - self.mask) * (inputs * s + t)
         
         # add log-det of Jacobian to the NEGATIVE log-likelihood loss
-        self.add_loss(
-            tf.reduce_mean(
-                -tf.reduce_sum(s * (1. - self.mask), axis=[1, 2, 3])
-            )
-        )
+        self._log_det_jacobian = tf.reduce_sum(s * (1. - self.mask), axis=[1, 2, 3])
 
         return y
 
@@ -67,25 +98,20 @@ class AffineCoupling(L.Layer):
         return x
 
 
-class Rescaling(L.Layer):
+class Rescaling(CouplingBase):
     def __init__(self):
         super(Rescaling, self).__init__()
     
+    @property
+    def log_det_jacobian(self):
+        return tf.reduce_sum(self.log_s)
+
     def build(self, input_shape):
-        self.log_s = tf.Variable(initial_value=tf.zeros_initializer()(shape=input_shape[1:], dtype="float32"), trainable=True)
+        self.log_s = tf.Variable(initial_value=tf.zeros_initializer()(shape=[1, input_shape[1]], dtype="float32"), trainable=True)
+        super(Rescaling, self).build(input_shape)
     
     def call(self, inputs):
-        # log-det of Jacobian
-        self.add_loss(-tf.reduce_sum(self.log_s))  # note: NEGATIVE log-likelihood
         return inputs * tf.exp(self.log_s)
 
     def inverse(self, inputs):
         return inputs * tf.exp(-self.log_s)
-
-class Reverse(L.Layer):
-    def call(self, inputs):
-        return tf.reverse(inputs, axis=[-1])
-
-    def inverse(self, inputs):
-        return tf.reverse(inputs, axis=[-1])
-

model/nice.py
@@ -3,55 +3,59 @@ import tensorflow.keras as K
 import tensorflow.keras.layers as L
 import numpy as np
 from model.layers import AdditiveCoupling
-from model.layers import Reverse
 from model.layers import Rescaling
 
-
 class NICE(K.Model):
-    def __init__(self, data_shape, n_coupling):
+    def __init__(self, data_shape, n_coupling, regularize=False):
         super(NICE, self).__init__()
         self.loss_tracker = K.metrics.Mean(name="loss")
         # currently 1-dim data assumed
         assert type(data_shape) == int
         self.data_shape = data_shape
+        self.regularize = regularize
 
         # model def
         self.couplings = []
-        for _ in range(n_coupling):
-            m = K.Sequential([L.Dense(128, activation="relu", kernel_initializer="he_normal"), 
-                              L.Dense(128, activation="relu", kernel_initializer="he_normal"), 
-                              L.Dense(128, activation="relu", kernel_initializer="he_normal"), 
-                              L.Dense(128, activation="relu", kernel_initializer="he_normal"), 
-                              L.Dense(data_shape // 2)])
-            self.couplings.append(AdditiveCoupling(m=m))
-        self.reverse_layer = Reverse()
-        self.rescaling = Rescaling()
+        for i in range(n_coupling):
+            m = self._build_network()
+            self.couplings.append(AdditiveCoupling(i % 2, m))
+        self.couplings.append(Rescaling())
 
     def call(self, inputs):
         h = inputs
         for layer in self.couplings:
             h = layer(h)
-            h = self.reverse_layer(h)
-        h = self.rescaling(h)
         return h
+
+    def _build_network(self):
+        if self.regularize:
+            dense = lambda n: L.Dense(n, activation="relu", kernel_initializer="he_normal", kernel_regularizer="l1", bias_regularizer="l1")
+        else:
+            dense = lambda n: L.Dense(n, activation="relu", kernel_initializer="he_normal")
+        # 4 hidden layer rectified network       
+        m = K.Sequential([dense(1000) for _ in range(4)])
+        m.add(L.Dense(self.data_shape // 2))
+        return m
 
     def _loss_f(self, h):
         # negetive log-likelihood of standard normal prior
-        # loss = 0.5 * tf.reduce_mean(
-        #     tf.reduce_sum(
-        #         tf.square(h) + tf.math.log(tf.constant(np.pi, dtype="float64") * 2.), 
-        #         axis=-1
-        #     )
+        # loss = 0.5 * tf.reduce_sum(
+        #    tf.square(h) + tf.math.log(tf.constant(np.pi, dtype="float32") * 2.), 
+        #    axis=-1
         # )
 
-        # logistic prior
-        loss = tf.reduce_mean(
-            tf.reduce_sum(
-                tf.math.log(1 + tf.exp(h)) + tf.math.log(1 + tf.exp(-h)), 
+        # logistic prior 
+        # note: use softplus to improve stability
+        loss = tf.reduce_sum(
+                tf.nn.softplus(h) + tf.nn.softplus(-h), 
                 axis=-1
-            )
         )
-        # Additive Coupling log-det = 1
+
+        # note: Additive Coupling log-det = 0
+        for layer in self.couplings:
+            loss += -layer.log_det_jacobian  # note: NEGATIVE log likelihood
+
+        loss = tf.reduce_mean(loss)
         return loss
 
     @property
@@ -61,18 +65,24 @@ class NICE(K.Model):
     def train_step(self, data):
         with tf.GradientTape() as tape:
             z = self(data, training=True)
+            tf.debugging.assert_all_finite(z, "z has nan/inf")
+            # compute NLL for each data point
             loss = self._loss_f(z)
+            tf.debugging.assert_all_finite(loss, "loss has nan/inf")
+            # metrics (NLL)
+            self.loss_tracker.update_state(loss)
+            # add other losses such as regularization
             loss += sum(self.losses)  
+            
         grads = tape.gradient(loss, self.trainable_variables)
-        # metrics (NLL)
-        self.loss_tracker.update_state(loss)
+        for g in grads:
+            tf.debugging.assert_all_finite(g, "grad has nan/inf")
         self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
 
         return { m.name : m.result() for m in self.metrics }
 
     def inverse(self, inputs):
-        h = self.rescaling.inverse(inputs)
+        h = inputs
         for layer in reversed(self.couplings):
-            h = self.reverse_layer.inverse(h)
             h = layer.inverse(h)
         return h

model/realnvp.py
@@ -53,12 +53,17 @@ class SimpleRealNVP(K.Model):
 
     def _loss_f(self, h):
         # nll of standard normal prior
-        loss = 0.5 * tf.reduce_mean(
-            tf.reduce_sum(
+        loss = 0.5 * tf.reduce_sum(
                 tf.square(h) + tf.math.log(tf.constant(np.pi, dtype="float32") * 2.),
                 axis=[1, 2, 3]  # sum over all pixels
-            )
         )
+
+        # log determinant
+        for layer in self.couplings:
+            loss += -layer.log_det_jacobian
+
+        # note: use mean (not sum) for stability
+        loss = tf.reduce_mean(loss)
         return loss
 
     @property

train.py
@@ -1,7 +1,10 @@
 import argparse
+import os
+import sys
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras as K
+import matplotlib.pyplot as plt
 import model
 import callbacks
 
@@ -20,8 +23,12 @@ if __name__ == "__main__":
     dataset = K.datasets.mnist
     (x_train, _), (x_test, _) = dataset.load_data()
     x_train = x_train.astype(np.float32)
-    # reduce the impact of boundary effects
-    x_train = .05 + .95 * x_train / 256.
+    # dequantization (?)
+    x_train = x_train + np.random.normal(size=x_train.shape)
+    x_train = (x_train - np.min(x_train)) / (np.max(x_train) - np.min(x_train))
+    # reduce the impact of boundary effects (?)
+    x_train = .05 + .95 * x_train
+
     # reshape to fit inputs of the model
     if args.model == "nice":
         x_train = np.reshape(x_train, [-1, 28*28])
@@ -29,7 +36,7 @@ if __name__ == "__main__":
         x_train = np.reshape(x_train, [-1, 28, 28, 1])
     # tf dataset
     x_tensor = tf.data.Dataset.from_tensor_slices(x_train)
-    x_tensor = x_tensor.batch(32)
+    x_tensor = x_tensor.batch(64)
 
     # latent dim
     if args.model == "nice":
@@ -46,9 +53,23 @@ if __name__ == "__main__":
 
     # train the model
     if args.model == "nice":
-        mdl = model.NICE(28 * 28, 4)
+        mdl = model.NICE(28 * 28, 4, regularize=True)
     elif args.model == "realnvp":
         mdl = model.SimpleRealNVP([28, 28, 1], 8)
     # compile
-    mdl.compile(optimizer=K.optimizers.Adam(1e-3, beta_1=0.9, beta_2=0.9, epsilon=1e-4))
-    mdl.fit(x_tensor, epochs=100, callbacks=[callback])
+    mdl.compile(optimizer=K.optimizers.Adam(1e-7, beta_1=0.9, beta_2=0.01, epsilon=1e-4))
+    mdl.fit(x_tensor, epochs=500, callbacks=[callback])
+
+    # save the model
+    save_dir = os.path.join(args.logdir, "saved_model")
+    if os.path.exists(save_dir):
+        print(f"{save_dir} already exists. Overwrite? [y/N]")
+        c = input()
+        if not c.upper() == "Y":
+            print("abort")
+            exit()
+
+    # make directory to save the model
+    os.makedirs(save_dir)
+
+    mdl.save(os.path.join(save_dir, "model"))